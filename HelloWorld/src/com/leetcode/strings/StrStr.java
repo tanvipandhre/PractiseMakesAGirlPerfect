6cc89dacbf792fa860431e7e43f767e75aff9625
tanvipandhre <37935899+tanvipandhre@users.noreply.github.com>
2022-01-15 22:55:44 +0530

    Create StrStr.java

---
+++ HelloWorld/src/com/leetcode/strings/StrStr.java
@@ -0,0 +1,15 @@
+package com.leetcode.strings;
+
+public class StrStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String haystack ="";
+		String needle ="";
+		int output=0;
+		output = haystack.indexOf(needle);
+		System.out.println(output);
+		
+	}
+
+}
