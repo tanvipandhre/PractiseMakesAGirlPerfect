2aa379cd237da09cd0d6cdb1d40b2d7464858751
tanvipandhre <37935899+tanvipandhre@users.noreply.github.com>
2022-01-17 18:52:45 +0530

    17012022_02
    17012022_02

---
+++ HelloWorld/src/com/leetcode/strings/RunLengthEncoding.java
@@ -0,0 +1,32 @@
+package com.leetcode.strings;
+
+public class RunLengthEncoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="aaabbc";
+		String out = "";
+		int count =0;
+		Character x = s.charAt(0);
+		out += s.charAt(0);
+		for(int i=0;i<s.length();i++) {
+			
+			if(s.charAt(i)!=x) {
+				x=s.charAt(i);
+				out+=count;
+				out+=s.charAt(i);
+				count=1;
+				continue;
+				
+			}
+			//out+=s.charAt(i);
+			count++;
+			
+			//
+			//System.out.print(out);
+		}
+		
+		System.out.println(out+count);
+	}
+
+}
