f8ac256dfbab95fc783fdfb4121d0d4d68a2fae4
tanvipandhre <37935899+tanvipandhre@users.noreply.github.com>
2022-02-08 01:17:33 -0500

    Overriding_07022022
    Overriding_07022022

---
+++ HelloWorld/src/com/njit/cs602/java/Thought.java
@@ -0,0 +1,8 @@
+package com.njit.cs602.java;
+
+public class Thought {
+	public void message() {
+		System.out.println("I feel like I'm diagonally parked in a "+"parallel universe");
+		System.out.println();
+	}
+}
